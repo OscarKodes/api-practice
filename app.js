7db6b100e8456cbd50ca5411491f43112a01842b
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-01 09:59:28 -0400

    Add PokeAPI and search by ID functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const request = require("request");
+const ejs = require("ejs");
 
 const app = express();
 
@@ -7,18 +8,31 @@ app.set('view engine','ejs');
 
 app.get("/", function(req, res){
 
-  request('https://pokeapi.co/api/v2/pokemon/ditto/', function (error, response, body) {
+  res.render("index");
+});
+
+app.get("/results", function(req, res){
+  let id = req.query.id;
+  let url = 'https://pokeapi.co/api/v2/pokemon/' + id;
+
+  request(url, function (error, response, body) {
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     if (error) {
       console.log('error:', error); // Print the error if one occurred
     } else {
-      let dataObj = JSON.parse(body);
+      let pokemonObj = JSON.parse(body);
 
-      res.render("index", {sprites: dataObj.sprites})
-
+      res.render("results", {
+        sprites: pokemonObj.sprites,
+        name: pokemonObj.name,
+        id: id
+      });
     }
   });
+
 });
+
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
